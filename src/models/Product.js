c5567b719d0a593ec6e46ac32891b358e529d4fa
anna-ciuvalscaia <67141302+anna-ciuvalscaia@users.noreply.github.com>
2021-08-05 11:36:15 +0300

    Add files via upload

---
+++ src/models/Product.js
@@ -0,0 +1,19 @@
+class Product {
+    constructor (id, name, imageUrl){
+this.id = id;
+this.name = name;
+this.imageUrl = imageUrl;
+    }
+    toPOJO() {
+return {
+    id: this.id,
+    name: this.name,
+    imageUrl: this.imageUrl
+}
+    }
+    toJSON(){
+     return JSON.stringify(this.toPOJO())   
+    }
+}
+
+export default Product
